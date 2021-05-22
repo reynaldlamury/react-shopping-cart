e4e942418400b1c1cac1ccf7631498a9a8bcd07c
Reynald Lamury <reynaldlamury@gmail.com>
2021-05-22 21:01:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { css } from 'styled-components';
+import './global.css';
 
-function App() {
+const GridContainer = styled.section`
+  display: grid;
+  grid-template-areas:
+    'header'
+    'main'
+    'footer';
+  grid-template-rows: 5rem 1fr 5rem;
+  grid-template-columns: 1fr;
+  height: 100%;
+`;
+
+const Header = styled.header`
+  grid-area: 'header';
+  background-color: #5e42fc;
+  padding: 0.5rem;
+`;
+
+const Main = styled.main`
+  grid-area: 'main';
+  background-color: coral;
+`;
+
+const Footer = styled.footer`
+  grid-area: 'footer';
+  background-color: slateblue;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Logo = styled.a`
+  transition: all 0.4s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    color: #ff9047;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GridContainer>
+      <Header>
+        <strong>
+          <Logo>React Shopping Cart</Logo>
+        </strong>
+      </Header>
+      <Main>Product List</Main>
+      <Footer>all right is reserved</Footer>
+    </GridContainer>
   );
-}
+};
 
 export default App;
